d608c148f1f197bd94303fb0c774d61c1ba3c69c
xhwufung <xhwufung@connect.ust.hk>
2024-10-30 21:45:14 +0800

    feat: update the scripts

--- llm/gpt.py
+++ llm/gpt.py
@@ -7,7 +7,7 @@ import unittest
 class GPT(ChatLLM):
     def __init__(self, model_specific:str = "gpt-4o", api_key:str = "TBD"):
         super().__init__("ChatGPT")
-        self.api_key= "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"
+        self.api_key = ""
         self.client = OpenAI(api_key=self.api_key)
         self.specific_model = model_specific 
 
